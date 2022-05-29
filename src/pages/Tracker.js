708665bd45d7c2bd2123e63074a3e24c4627309f
belalsm <belalsm95@gmail.com>
2022-05-28 20:12:40 -0400

    timer and tracker item states

--- src/pages/Tracker.js
+++ src/pages/Tracker.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TrackerItem from '../components/TrackerItem'
 
 function Tracker() {
+    const initial = [{ id: 1, title: "test1" }, { id: 2, title: "test2" }]
+
+    const [trackingItems, setTrackingItems] = useState(initial);
+
+    function AddNewItem() {
+        setTrackingItems([...trackingItems, { id: 3, title: "test3" }]);
+    }
+
+    //how will you do it where when you add a new tracker item it creates an item with a blank title and promps for title?
+
     return (
         <div className='tracker'>
             <h2 className="tracker__title">
@@ -9,15 +19,13 @@ function Tracker() {
             </h2>
             <div className="tracker__add">
                 <h3>Add Tracking Category</h3>
-                <button>+</button>
+                <button onClick={() => AddNewItem()}>+</button>
             </div>
             <div className="tracker__items">
-                <TrackerItem />
-                <TrackerItem />
-                <TrackerItem />
+                {trackingItems && trackingItems.map(item => (
+                    <TrackerItem key={item.id} id={item.id} title={item.title} />
+                ))}
             </div>
-
-
         </div>
     )
 }
